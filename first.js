e7b5574b50fec8cb96a61a4b9d5d0eff1db2ac3e
Leonid <leoural66@yandex.ru>
2021-03-21 18:36:34 +0500

    step 1

---
+++ first.js
@@ -0,0 +1,26 @@
+// const http = require("http");
+// const os = require('os'); // модуль окружения и операционной системы
+// const greeting = require('./greeting'); //нащ модуль, подключение   
+// const User = require('./module/user');
+const welcome = require('./welcome');
+
+welcome.getMorningMessage();
+welcome.getEveningMessage();
+
+// let eugene = new User('Eugene', 32);
+// eugene.sayHi();
+
+
+// // http.createServer(function (request, response) {
+
+// let userName = os.userInfo().username;
+// console.log(userName + ' <<<<');
+
+// console.log(`Дата запроса: ${greeting.date}`);
+// console.log(greeting.getMessage(userName));
+
+// response.end("Hello NodeJS! " + userName);
+
+// }).listen(3000, 'localhost', function () {
+//     console.log('SERVER RUN on port:3000');
+// });
