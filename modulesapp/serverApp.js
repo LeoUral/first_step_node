5cc04504c53044cf1b602bd1db9d9f720ede0660
Leonid <leoural66@yandex.ru>
2021-06-12 15:52:49 +0500

    Update serverApp.js

--- modulesapp/serverApp.js
+++ modulesapp/serverApp.js
@@ -3,14 +3,29 @@ const http = require('http');
 // request - хранит информацию о запросе, response - управляет отправкой ответа
 http.createServer(function(request, response) {
 
-        console.log('Url: ' + request.url);
-        console.log('Тип запроса: ' + request.method);
-        console.log('User-Agent: ' + request.headers["user-agent"]);
-        console.log('Все заголовки');
-        console.log(request.headers);
+        // console.log('Url: ' + request.url);
+        // console.log('Тип запроса: ' + request.method);
+        // console.log('User-Agent: ' + request.headers["user-agent"]);
+        // console.log('Все заголовки');
+        // console.log(request.headers);
 
-        response.write('bla bla bla  bls \n');
-        response.end('Hello! WORLD');
+
+        // response.setHeader('UserId', 12);
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        response.write("<!DOCTYPE html>");
+        response.write("<html>");
+        response.write("<head>");
+        response.write("<title>Hello Node.js</title>");
+        response.write("<meta charset=\"utf-8\" />");
+        response.write("</head>");
+        response.write("<body>");
+        response.write("<h1>Привет миг</h1>");
+        response.write("<h2>Привет! Учим Node.js </h2>");
+        response.write("<div> Изучаем написание сервера на NODE.JS </div>")
+        response.write("</body>");
+        response.write("</html>");
+        // response.end('Hello! WORLD');
+        response.end();
     }
 
 ).listen(3000, () => { console.log('RUN Server'); });
