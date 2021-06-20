6e8cebf802351f9a00ea2478c57868552560c63b
Leonid <leoural66@yandex.ru>
2021-06-20 11:50:03 +0500

    Представления

--- expressapp/handleApp.js
+++ expressapp/handleApp.js
@@ -1,26 +1,53 @@
-//*Представления и движок Handlebars
-const express = require('express');
+//*Частичные представления
 
+const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
 app.set('view engine', 'hbs');
-app.set('views', 'templates'); //установка пути к предсталениям
+//Для настройки функционала частичных представлений в коде используется вызов:
+hbs.registerPartials(__dirname + '/views/partials');
+//который устанавливает каталог, где располагаются частичные представления.
 
 app.use('/contact', (request, response) => {
-
-    response.render('contact1.hbs', {
-        title: 'Contacts',
-        emailsVisible: true,
-        emails: ['leo@gmail.com', 'leo@mail.ru'],
-        phone: '+7-111-222-4455'
+    response.render('contact2.hbs', {
+        title: 'May Contacts',
+        email: 'leo@mail.ru',
+        phone: '+7-999-888-7766'
     });
 });
 
 app.use('/', (request, response) => {
-    response.send('Main Page three');
+    response.render('home.hbs');
 });
 
-app.listen(3000, () => { console.log('run server'); });
+app.listen(3000, () => { console.log('server run'); });
+
+
+
+// //*Представления и движок Handlebars
+// const express = require('express');
+
+// const app = express();
+
+// app.set('view engine', 'hbs');
+// app.set('views', 'templates'); //установка пути к предсталениям
+
+// app.use('/contact', (request, response) => {
+
+//     response.render('contact1.hbs', {
+//         title: 'Contacts',
+//         emailsVisible: true,
+//         emails: ['leo@gmail.com', 'leo@mail.ru'],
+//         phone: '+7-111-222-4455'
+//     });
+// });
+
+// app.use('/', (request, response) => {
+//     response.send('Main Page three');
+// });
+
+// app.listen(3000, () => { console.log('run server'); });
 
 
 // //*Представления и движок Handlebars
